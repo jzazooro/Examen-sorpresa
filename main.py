11f88c9542185160bee4c4d3e618dbeba0af79ab
jzazooro <91785177+jzazooro@users.noreply.github.com>
2022-04-08 12:09:56 +0200

    clase aire bien

---
+++ main.py
@@ -0,0 +1,97 @@
+from clases.weapon_type import *
+from clases.pokemon import *
+from clases.pokemon_air import *
+from clases.pokemon_electricity import *
+from clases.pokemon_water import *
+from clases.pokemon_earth import*
+
+def main():
+    pokemon_1 = PokemonAir(1, "Pidgey", WeaponType.HEADBUTT, 100, 8, 7)
+
+    if pokemon_1.get_pokemon_name() == "Pidgey":
+        print("Test PASS. The parameter pokemon_name has been correctly set.")
+    else:
+        print("Test FAIL. Check the method __init__().")
+
+    if pokemon_1.get_weapon_type().name == "HEADBUTT":
+        print("Test PASS. The parameter weapon_type has been correctly set.")
+    else:
+        print("Test FAIL. Check the method __init__().")
+
+    if pokemon_1.get_health_points() == 100:
+        print("Test PASS. The parameter health_points has been correctly set.")
+    else:
+        print("Test FAIL. Check the method __init__().")
+
+    if pokemon_1.get_attack_rating() == 8:
+        print("Test PASS. The parameter attack_rating has been correctly set.")
+    else:
+        print("Test FAIL. Check the method __init__().")
+
+    if pokemon_1.get_defense_rating() == 7:
+        print("Test PASS. The parameter defense_rating has been correctly set.")
+    else:
+        print("Test FAIL. Check the method __init__().")
+    pokemon_2 = PokemonAir(7, "Pidgey", WeaponType.HEADBUTT, 100, 7, 6)
+
+    if str(pokemon_2) == "Pokemon ID 7 with name Pidgey has as weapon HEADBUTT and health 100":
+        print("Test PASS. The human-readable format of the object has been implemented correctly.")
+    else:
+        print("Test FAIL. Check the method __str__()." + " RESULT: " + str(pokemon_2))
+    pokemon_3 = PokemonAir(3, "Pidgey", WeaponType.KICK, 97, 8, 7)
+
+    if pokemon_3.is_alive():
+        pokemon_was_hit = pokemon_3.fight_defense(200)  # With this the Pokemon should be retired.
+
+        if pokemon_was_hit:
+            if not pokemon_3.is_alive():
+                print("Test PASS. The method is_alive() has been implemented correctly.")
+            else:
+                print("Test FAIL. Check the method is_alive().")
+        else:
+            if pokemon_3.is_alive():
+                print("Test PASS. The method is_alive() has been implemented correctly.")
+            else:
+                print("Test FAIL. Check the method is_alive().")
+            
+    else:
+        print("Test FAIL. Check the method is_alive().")
+    pokemon_4 = PokemonAir(4, "Pidgey", WeaponType.ELBOW, 93, 9, 5)
+
+    pokemon_was_hit = pokemon_4.fight_defense(70)
+
+    if pokemon_was_hit:
+        if pokemon_4.get_health_points() == 28:
+            print("Test PASS. The method fight_defense() has been implemented correctly.")
+        else:
+            print("Test FAIL. Check the method fight_defense().")
+    else:
+        if pokemon_4.get_health_points() == 93:
+            print("Test PASS. The method fight_defense() has been implemented correctly.")
+        else:
+            print("Test FAIL. Check the method fight_defense().")
+    pokemon_5 = PokemonAir(5, "Pidgey", WeaponType.PUNCH, 99, 10, 8)
+    pokemon_6 = PokemonAir(6, "Pidgey", WeaponType.PUNCH, 99, 9, 6)
+
+    pokemon_was_hit = pokemon_5.fight_attack(pokemon_6)
+
+    if pokemon_was_hit:
+        if pokemon_6.get_health_points() == 95:
+            print("Test PASS. The method fight_attack() has been implemented correctly.")
+        else:
+            print("Test FAIL. Check the method fight_attack().")
+    else:
+        if pokemon_6.get_health_points() == 99:
+            print("Test PASS. The method fight_attack() has been implemented correctly.")
+        else:
+            print("Test FAIL. Check the method fight_attack().")
+
+
+            
+
+
+
+
+
+if __name__ == "__main__":
+    main()
